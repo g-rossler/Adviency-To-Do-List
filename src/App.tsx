a96f7928207556e3b5596a4960e1873f26d55afd
Rossler Gerardo <60127824+g-rossler@users.noreply.github.com>
2022-04-19 12:51:33 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Regalos:</h1>
+      <ul>
+        <li>...</li>
+      </ul>
+    </div>
+  );
+}
