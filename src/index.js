e18b020691edb77b3e8f9f2b8af44355544f0883
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-07-11 13:25:49 -0300

    Fix dark mode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { render } from "react-dom";
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
-import theme from './themes/index'
+import theme from "./themes/index";
 
 const rootElement = document.getElementById("root");
-render(<ChakraProvider>
-	<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-		<App />
-	</ChakraProvider>, rootElement);
+render(
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>,
+  rootElement
+);
--- src/themes/index.js
+++ src/themes/index.js
@@ -1,9 +1,10 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
 
-const theme = extendTheme({
-  config: {
-    initialColorMode: 'dark'
-  },
-})
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
 
-export default theme
+const theme = extendTheme({ config });
+
+export default theme;
