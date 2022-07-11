5d46fee9a7de5130be5f10cc4f2df8a531abe94a
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-07-11 11:45:02 -0300

    Add ColorMode

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { render } from "react-dom";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from "./App";
+import theme from './themes/index'
 
 const rootElement = document.getElementById("root");
 render(<ChakraProvider>
+	<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 		<App />
 	</ChakraProvider>, rootElement);
